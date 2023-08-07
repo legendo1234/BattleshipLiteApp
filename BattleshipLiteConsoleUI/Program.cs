184fca99bb569cb787eb4682ec39ba8a0a40a7d7
legendo1234 <matej.weber@gmail.com>
2023-08-07 11:25:00 +0200

    Made samlpe methods for logic, finished interface

--- BattleshipLiteConsoleUI/Program.cs
+++ BattleshipLiteConsoleUI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using BattleshipLiteLibrary;
 using BattleshipLiteLibrary.Models;
+using System.Data.Common;
 
 class Program
 {
@@ -7,12 +8,113 @@ class Program
     {
         WelcomeMessage();
 
-        PlayerInfoModel player1 = CreatePlayer("Player 1 ");
-        PlayerInfoModel player2 = CreatePlayer("Player 2 ");
+        PlayerInfoModel activePlayer = CreatePlayer("Player 1 ");
+        PlayerInfoModel opponent = CreatePlayer("Player 2 ");
+
+        PlayerInfoModel winner = null;
+
+        do
+        {
+          
+            DisplayShotGrid(activePlayer);
+           
+            RecordPlayerShot(activePlayer, opponent);
+
+            bool doesGameContinue = GameLogic.PlayerStillActive(opponent);
+
+
+            if (doesGameContinue == true)
+            {
+                // Swap positions
+                (activePlayer, opponent) = (opponent, activePlayer);
+
+
+            }
+            else
+            {
+                winner = activePlayer;
+            }
+
+
+        } while (winner == null);
+
+        IdentifyWinner(winner);
 
         Console.ReadLine();
     }
 
+    private static void IdentifyWinner(PlayerInfoModel winner)
+    {
+        Console.WriteLine($"Congratulations to {winner.UsersName} for winning!");
+        Console.WriteLine($"{winner.UsersName} took {GameLogic.GetShotCount(winner)} shots. ");
+    }
+
+    private static void RecordPlayerShot(PlayerInfoModel activePlayer, PlayerInfoModel opponent)
+    {
+        bool isValidShot = false;
+        string row = " ";
+        int column = 0;
+
+        do
+        {
+            string shot = AskForShot();
+            ( row, column) = GameLogic.SplitShotIntoRowAndColumn(shot);
+            isValidShot = GameLogic.ValidateShot(activePlayer, row, column);
+
+            if (isValidShot == false)
+            {
+                Console.WriteLine("Invalid Shot location. Please try again.");
+            }
+
+        } while (isValidShot == false);
+
+        bool isaAHit = GameLogic.IdentifyShotResult(opponent, row, column);
+
+        GameLogic.MarkShotResult(activePlayer, row, column, isaAHit);
+
+    }
+
+    private static string AskForShot()
+    {
+        Console.Write("Please enter your shot selection");
+        string output = Console.ReadLine();
+
+        return output;
+    }
+
+    private static void DisplayShotGrid(PlayerInfoModel activePlayer)
+    {
+        string currentRow = activePlayer.SpotGrid[0].SpotLetter;
+
+        foreach (var gridSpot in activePlayer.SpotGrid)
+        {
+            if (gridSpot.SpotLetter != currentRow)
+            {
+                Console.WriteLine();
+                currentRow = gridSpot.SpotLetter;
+            }
+
+            if (gridSpot.Status == GridSpotStatus.Empty)
+            {
+                Console.Write($"{gridSpot.SpotLetter}{gridSpot.SpotNumber} ");
+            }
+
+            else if (gridSpot.Status == GridSpotStatus.Hit)
+            {
+                Console.Write(" X ");
+            }
+
+            else if(gridSpot.Status == GridSpotStatus.Miss)
+            {
+                Console.Write(" O ");
+            }
+
+            else
+            {
+                Console.Write(" ? ");
+            }
+        }
+    }
 
     private static void WelcomeMessage()
     {
@@ -56,7 +158,7 @@ class Program
     {
         do
         {
-            Console.Write($"Where do you want to place your next ship number{model.ShipLocations.Count + 1} : ");
+            Console.Write($"Where do you want to place your next ship number  {model.ShipLocations.Count + 1} : ");
             string location = Console.ReadLine();
 
             bool isValidLocation = GameLogic.PlaceShip(model,location);
